b6d6185e62b1725c99bef3796384898f07e1732a
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-02-08 12:30:13 -0500

    Add mobile responsiveness and Google fonts via helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def include_fonts
+		fonts = <<-sitefonts      
+      <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,400italic' rel='stylesheet' type='text/css'>
+      <link href='http://fonts.googleapis.com/css?family=Arapey:400italic,400' rel='stylesheet' type='text/css'>
+		sitefonts
+		
+		fonts.html_safe
+	end
 end
