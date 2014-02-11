317ccd1c202358baf18798f027e6f88d49367bce
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-02-11 00:39:53 -0500

    Add modal support and begin styling.

--- app/assets/javascripts/flow-custom.js
+++ app/assets/javascripts/flow-custom.js
@@ -1,3 +1,4 @@
+// Add scroll effect to savings-btn.
 $('#savings-btn').click(function () {
     $('html,body').animate({
         scrollTop: $("#purchase").offset().top
@@ -5,3 +6,17 @@ $('#savings-btn').click(function () {
     	$('.active').removeClass();
     });
 });
+
+// Create function for binding JS modals.
+function initModal(link_id, popup_id){
+	$(link_id).bind('click', function(e) {
+    // Prevents the default action to be triggered. 
+    e.preventDefault();
+
+    // Triggering bPopup when click event is fired
+    $(popup_id).bPopup();
+	});
+}
+
+initModal('#privacy-l', '#privacy-p');
+initModal(  '#terms-l',   '#terms-p');
