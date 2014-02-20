65aa72f788fce9cd5dd3fb22ac2b0229b6b83c5c
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-02-19 22:58:45 -0500

    Fix first round of feedback for Flow website.

--- app/assets/javascripts/flow-custom.js
+++ app/assets/javascripts/flow-custom.js
@@ -1,4 +1,13 @@
-// Add scroll effect to savings-btn.
+// Add scroll effect:
+//   Learn More button on intro page.
+$('#learn-btn').click(function () {
+    $('html,body').animate({
+        scrollTop: $("#why").offset().top
+    }, 1000);
+});
+
+// Add scroll effect:
+//   Savings button on nav bar.
 $('#savings-btn').click(function () {
     $('html,body').animate({
         scrollTop: $("#purchase").offset().top
