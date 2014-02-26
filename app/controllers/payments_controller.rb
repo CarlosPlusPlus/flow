160e061167be636baa86047e0b7283f00f446f7a
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-02-26 18:53:59 -0500

    Link up form, controller, and javascript flow for initial service post.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,6 +2,8 @@ class PaymentsController < ApplicationController
   
   # JSON Post of Payment Info for iframe generation.
   def iframe
-    render :json => "iframe test string"
+    @iframe_str = "iframe test string!"
+
+    respond_to { |format| format.js }
   end
 end
