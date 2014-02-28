f7a412b1a83176f3561751722cba4c83a1e499f5
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-02-28 00:17:30 -0500

    Add in JavaScript counters to About Flow section.

--- app/assets/javascripts/flow-custom.js
+++ app/assets/javascripts/flow-custom.js
@@ -26,4 +26,21 @@ $('document').ready(function(){
   $('.faq .question .collapse').on('hide.bs.collapse', function (n) {
       $(n.target).siblings('.faq-toggle').toggleClass('open');        
   });
+
+  // Add a counter function to compute gallons / money saved.
+  function flowCounter() {
+    var curr = new Date(); 
+    var past = new Date('1/1/2012');
+    var days = ((Math.abs(curr.getTime()) - Math.abs(past.getTime()))/60/60/24/1000).toFixed(5)
+    
+    var customers = 10000;
+    var water_per_day = customers * 25;   // 100 gallons per day @ 25% savings
+    var money_per_day = customers * 0.82; // $3.30 per day       @ 25% savings
+    
+    document.getElementById("gallons-saved").innerHTML =       (customers * water_per_day * days).toFixed(0);
+    document.getElementById("dollars-saved").innerHTML = '$' + (customers * money_per_day * days).toFixed(0);
+  };
+
+  setInterval(function() { flowCounter() }, 1000);
+
 });
