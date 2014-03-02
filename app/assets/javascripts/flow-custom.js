aefa2326686a60446d3704fb819d712facbe67b9
Adam Waxman <awaxman11@gmail.com>
2014-03-02 17:39:11 -0500

    Format and adjust savings numbers

--- app/assets/javascripts/flow-custom.js
+++ app/assets/javascripts/flow-custom.js
@@ -31,14 +31,14 @@ $('document').ready(function(){
   function flowCounter() {
     var curr = new Date(); 
     var past = new Date('1/1/2012');
-    var days = ((Math.abs(curr.getTime()) - Math.abs(past.getTime()))/60/60/24/1000).toFixed(5)
+    var days = ((Math.abs(curr.getTime()) - Math.abs(past.getTime()))/60/60/24/1000).toFixed(5) / 40
     
-    var customers = 10000;
-    var water_per_day = customers * 25;   // 100 gallons per day @ 25% savings
-    var money_per_day = customers * 0.82; // $3.30 per day       @ 25% savings
+    var c = 1000;
+    var water_per_day = c * 24.325;   // avg s
+    var money_per_day = c * 0.82;     // avg $
     
-    document.getElementById("gallons-saved").innerHTML =       (customers * water_per_day * days).toFixed(0);
-    document.getElementById("dollars-saved").innerHTML = '$' + (customers * money_per_day * days).toFixed(0);
+    document.getElementById("gallons-saved").innerHTML =       Math.round((c * water_per_day * days)).toLocaleString(0);
+    document.getElementById("dollars-saved").innerHTML = '$' + Math.round((c * money_per_day * days)).toLocaleString(0);
   };
 
   setInterval(function() { flowCounter() }, 1000);
