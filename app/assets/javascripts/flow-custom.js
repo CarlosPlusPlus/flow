981ccab3f234e61db1f4a221aa8e22a6b22d1ac2
Adam Waxman <awaxman11@gmail.com>
2014-03-04 23:26:43 -0500

    JS tweaks

--- app/assets/javascripts/flow-custom.js
+++ app/assets/javascripts/flow-custom.js
@@ -4,18 +4,8 @@ $('document').ready(function(){
   //   Learn More button on intro page.
   $('#learn-btn').click(function () {
     $('html,body').animate({
-      scrollTop: $("#why").offset().top
+      scrollTop: $("#why").offset().top - 85
     }, 1000);
-  });
-
-  // Add scroll effect:
-  //   Savings button on nav bar.
-  $('#savings-btn').click(function () {
-    $('html,body').animate({
-      scrollTop: $("#purchase").offset().top
-    }, 2000, function(){
-    	$('.active').removeClass();
-    });
   });
 
   // Collapse effects for FAQs.
