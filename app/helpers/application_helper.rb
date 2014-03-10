a61bd60a64f6d15f0278936d3b5224a026d6de69
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-03-09 23:41:23 -0400

    Update success text with new copy and change site to LIVE mode.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
 		current_page?(root_url)
 	end
 
+	def confirmation?
+		current_page?(page_path('success'))
+	end
+
 	def include_fonts
 		fonts = <<-sitefonts      
       <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,400italic' rel='stylesheet' type='text/css'>
