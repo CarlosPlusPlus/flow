bcf1e092f6dbdaf1e8821a3df51ba416486598b2
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-06-29 14:28:04 -0400

    Adds coupon code validation within PaymentsController.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,15 +1,9 @@
 class PaymentsController < ApplicationController
-  
-  def couponcode
-    # TODO: Check params[:couponcode] via REGEX (application.yml)
-    # => If yes, in ApplicationController:
-    # ==> Set @discount == true, @couponcode = params[:couponcode]
-    # => Return 'success' || 'invalid' based on status.
-    # *** DO this in PaymentsController as private methods.
-    @status = "success"
+  before_action :check_coupon_code, only: :couponcode
 
+  def couponcode
     respond_to do |format|
-      format.json { render json: { :status => @status }}
+      format.json { render json: { :discount => @discount }}
     end
   end
 
@@ -18,8 +12,24 @@ class PaymentsController < ApplicationController
     # TODO: @discount ? ENV['FLOW_DISCOUNT_PRICE'] || ENV['FLOW_PRICE']
     # TODO: Comment1 => "" || getCouponCode
     @iframe = Paypal::authenticate_iframe(params.merge(root_url: root_url))
-    # TODO: Reset ApplicationController @discount & @couponcode Value
+    reset_coupon_variables
 
     respond_to { |format| format.js }
+  end
+
+  private
+
+  def check_coupon_code
+    session[:coupon_code] = params['code'] || ""
+    session[:discount]    = valid_code?(session[:coupon_code])
+  end
+
+  def reset_coupon_variables
+    session[:coupon_code] = ""
+    session[:discount]    = false
+  end
+
+  def valid_code?(coupon_code)
+    (coupon_code =~ /#{ENV['FLOW_REGEX']}/) ? true : false
   end
 end
